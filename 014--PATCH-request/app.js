f5ba2064f1b8a69f61ec363baa59a259428017ca
huyphamfc <huyphamfc@gmail.com>
2022-09-15 10:42:21 +0700

    handle PATCH requests

---
+++ 014--PATCH-request/app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const express = require('express');
+
+
+const app = express();
+app.use(express.json());
+
+const data = JSON.parse(
+    fs.readFileSync(`${__dirname}/data.json`)
+);
+
+app.patch('/user/:id', (req, res) => {
+    const id = req.params.id * 1;
+
+    if (id < 0 || id >= data.length) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+        return;
+    }
+
+    res.status(200).end('Done');
+});
+
+app.listen(8000, err => {
+    if (err) throw err;
+    console.log('Listening on port 8000 ...');
+});
