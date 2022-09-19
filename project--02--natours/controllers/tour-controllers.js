a3f7ba45c5f3ec579421a056c31ebfd9a28ef92b
huyphamfc <huyphamfc@gmail.com>
2022-09-19 14:01:15 +0700

    Natours - get all Tours

--- project--02--natours/controllers/tour-controllers.js
+++ project--02--natours/controllers/tour-controllers.js
@@ -16,3 +16,18 @@ exports.createTour = async (req, res) => {
         });
     }
 }
+
+exports.getAllTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: 'success',
+            data: { tours }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+}
--- project--02--natours/routers/tour-routers.js
+++ project--02--natours/routers/tour-routers.js
@@ -3,6 +3,7 @@ const tourController = require('../controllers/tour-controllers');
 
 
 const router = express.Router();
+router.route('/').get(tourController.getAllTours);
 router.route('/').post(tourController.createTour);
 
 
