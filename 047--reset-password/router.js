7efd34ff069c7761abef11b549a3c102276eff2c
huyphamfc <huyphamfc@gmail.com>
2022-10-03 20:50:25 +0700

    forgot password

---
+++ 047--reset-password/router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const accountControllers = require('./controllers');
+
+const accountRouter = express.Router();
+
+accountRouter.route('/signup').post(accountControllers.signup);
+
+accountRouter.route('/login').post(accountControllers.login);
+
+accountRouter.route('/forgot-password').post(accountControllers.forgotPassword);
+
+accountRouter
+  .route('/')
+  .get(accountControllers.protectRoute, accountControllers.getAllAccounts);
+
+accountRouter
+  .route('/:email')
+  .get(accountControllers.protectRoute, accountControllers.getAccountByEmail)
+  .delete(
+    accountControllers.protectRoute,
+    accountControllers.restrictTo('admin'),
+    accountControllers.deleteAccountByEmail
+  );
+
+module.exports = accountRouter;
