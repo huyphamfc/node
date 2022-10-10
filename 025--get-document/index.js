94288e635a95649e2e1c3c021d7d6e65ab49d3ec
huyphamfc <huyphamfc@gmail.com>
2022-10-10 08:49:10 +0700

    get a Document - fix Schema

--- 025--get-document/index.js
+++ 025--get-document/index.js
@@ -2,39 +2,39 @@ const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 mongoose
-    .connect(DB)
-    .then(() => console.log('Connect DB successfully.'))
-    .catch(err => { throw err });
+  .connect(DB)
+  .then(() => console.log('Connect DB successfully.'))
+  .catch(err => console.log(err));
 
-const tourSchema = mongoose.Schema({ _id: String });
+const tourSchema = mongoose.Schema({});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
 const app = express();
 
 app.get('/tours/:id', async (req, res) => {
-    try {
-        const tour = await Tour.findById(req.params.id);
-        res.status(200).json({
-            status: 'success',
-            data: tour
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
+  try {
+    const tour = await Tour.find({ _id: req.params.id });
+
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 });
 
 app.listen(port, err => {
-    if (err) throw err;
-    console.log(`Listening on port ${port}...`);
+  if (err) throw err;
+  console.log(`Listening on port ${port}...`);
 });
