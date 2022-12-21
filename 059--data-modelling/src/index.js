21906708028b36ce23607c12b37d76d7c6012d8e
huyphamfc <huyphamfc@gmail.com>
2022-12-21 21:00:57 +0700

    connect MongoDB

--- 059--data-modelling/src/index.js
+++ 059--data-modelling/src/index.js
@@ -1,10 +1,16 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 dotenv.config();
-const PORT = process.env.PORT | 8000;
+const { MONGODB_URI } = process.env;
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log('Connect MongoDB successfully.'));
 
 app.listen(PORT, () => {
-  console.log(`Server running on port:${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });
